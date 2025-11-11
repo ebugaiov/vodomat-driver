5352086cc6f09241393e9220aff0e90e0aa3cdb5
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-11-11 12:55:11 +0100

    init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/app/ui/dashboard/navbar';
+
+export default async function Page() {
+    return (
+        <main>
+            <Navbar />
+            <h1>Dashboard page</h1>
+        </main>
+    )
+}
---
+++ app/lib/definitions.ts
@@ -0,0 +1,6 @@
+type Machine = {
+    address: string;
+    remind_water: number;
+    volume: number;
+    last_notification: string;
+}
