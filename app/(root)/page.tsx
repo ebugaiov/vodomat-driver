e9e32113947378294b79dead8bacc20dd0377cf5
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-11-17 13:01:08 +0100

    add needed packages and tiny hierachy updates

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar';
+import Statuses from '@/components/dashboard/statuses';
+
+export default async function Page() {
+    return (
+        <main className="pt-20">
+            <Navbar />
+            {/* <Statuses /> */}
+        </main>
+    )
+}
